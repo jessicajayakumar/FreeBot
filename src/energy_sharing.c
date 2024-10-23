783f2e4a67d362d21d5ac5570e1aecc70ff17151
Jessica Jayakumar <118928476+jessicajayakumar@users.noreply.github.com>
2024-10-23 14:04:00 +0100

    new files to write code to test freebot energy sharing

---
+++ src/energy_sharing.c
@@ -0,0 +1,5 @@
+// Basic energy sharing code
+// Does not utilise BLE connection to the DKB
+// Transmits voltage readings over the serial monitor which is then recorded and live plotted to show
+// voltage drop+ increase when energy is shared
+
